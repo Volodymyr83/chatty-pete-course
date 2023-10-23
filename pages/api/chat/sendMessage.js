f89c00d472b046da6c094d78d4657112830caec9
Volodymyr Lutsak <volodialutsak@gmail.com>
2023-10-23 09:50:46 +0300

    Lesson 41 complete

---
+++ pages/api/chat/sendMessage.js
@@ -0,0 +1,119 @@
+import { headers } from "next/dist/client/components/headers";
+import { OpenAIEdgeStream } from "openai-edge-stream";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req) {
+  try {
+    const { chatId: chatIdFromParam, message } = await req.json();
+    let chatId = chatIdFromParam;
+    console.log("MESSAGE:", message);
+    const initialChatMessage = {
+      role: "system",
+      content:
+        "Your name is Chatty Pete. An incredibly intelligent and quick-thinking AI, that always replies with an enthusiastic and positive energy. You were created by WebDevEducation. Your response must be formatted as a markdown",
+    };
+
+    let newChatId;
+    let chatMessages = [];
+
+    if (chatId) {
+      // add message to chat
+      const response = await fetch(
+        `${req.headers.get("origin")}/api/chat/addMessageToChat`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            cookie: req.headers.get("cookie"),
+          },
+          body: JSON.stringify({
+            chatId,
+            role: "user",
+            content: message,
+          }),
+        }
+      );
+      const json = await response.json();
+      chatMessages = json.chat.messages || [];
+    } else {
+      const response = await fetch(
+        `${req.headers.get("origin")}/api/chat/createNewChat`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            cookie: req.headers.get("cookie"),
+          },
+          body: JSON.stringify({ message }),
+        }
+      );
+      const json = await response.json();
+      chatId = json._id;
+      newChatId = json._id;
+      chatMessages = json.messages || [];
+    }
+
+    const messagesToInclude = [];
+    chatMessages.reverse();
+    let usedTokens = 0;
+    for (let chatMessage of chatMessages) {
+      const messageTokens = chatMessage.content.length / 4;
+      usedTokens += messageTokens;
+
+      if (usedTokens <= 2000) {
+        messagesToInclude.push(chatMessage);
+      } else {
+        break;
+      }
+    }
+
+    messagesToInclude.reverse();
+
+    const stream = await OpenAIEdgeStream(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [initialChatMessage, ...messagesToInclude],
+          stream: true,
+        }),
+      },
+      {
+        onBeforeStream: ({ emit }) => {
+          if (newChatId) {
+            emit(newChatId, "newChatId");
+          }
+        },
+        onAfterStream: async ({ fullContent }) => {
+          await fetch(
+            `${req.headers.get("origin")}/api/chat/addMessageToChat`,
+            {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+                cookie: req.headers.get("cookie"),
+              },
+              body: JSON.stringify({
+                chatId,
+                role: "assistant",
+                content: fullContent,
+              }),
+            }
+          );
+        },
+      }
+    );
+
+    return new Response(stream);
+  } catch (e) {
+    console.log("AN ERROR OCCURRED IN SENDMESSAGE: ", e);
+  }
+}
